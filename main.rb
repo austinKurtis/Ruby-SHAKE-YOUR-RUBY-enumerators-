39440563a102c4b4bbaa1b67b2c0b309a035412c
AustinJ <austin.jay.kurtis@gmail.com>
2017-10-16 15:43:10 -0500

    comple but not refactored

---
+++ main.rb
@@ -0,0 +1,141 @@
+flowers = ["Everybody clap", "Moonwalk", "Shopping Cart", "Robot", "Twist", "Cabbage Patch", "Raise the roof", "Sprinkler", "Running Man", "Worm", "Shake your Ruby"]
+e = flowers.to_enum
+puts e.next
+puts e.next
+puts e.next
+e.rewind
+e.next
+puts e.next
+puts e.rewind.next
+e.next
+e.next
+puts e.next
+puts e.next
+e.rewind.next
+e.next
+e.next
+puts e.next
+puts e.rewind.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
+puts e.rewind.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
+e.rewind.next
+e.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
+e.next
+puts e.next
+e.rewind.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
+e.next
+e.next
+e.next
+puts e.next
+puts e.rewind.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
+puts e.rewind.next
+puts e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
+e.rewind.next
+e.next
+e.next
+puts e.next
+e.next
+e.next
+e.next
+puts e.next
+puts e.rewind.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
+puts e.rewind.next
+e.next
+puts e.next
+e.rewind.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
+e.rewind.next
+e.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
+e.rewind.next
+e.next
+e.next
+e.next
+puts e.next
+e.rewind.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
+puts e.rewind.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+e.next
+puts e.next
