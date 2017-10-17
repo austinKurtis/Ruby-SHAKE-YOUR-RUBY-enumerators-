1bfb42cde5b550b9dde2f9a1cf1832229d23ccc7
AustinJ <austin.jay.kurtis@gmail.com>
2017-10-16 19:03:58 -0500

    refactored

--- main.rb
+++ main.rb
@@ -1,5 +1,8 @@
 flowers = ["Everybody clap", "Moonwalk", "Shopping Cart", "Robot", "Twist", "Cabbage Patch", "Raise the roof", "Sprinkler", "Running Man", "Worm", "Shake your Ruby"]
 e = flowers.to_enum
+
+	
+
 puts e.next
 puts e.next
 puts e.next
@@ -7,135 +10,92 @@ e.rewind
 e.next
 puts e.next
 puts e.rewind.next
-e.next
-e.next
+2.times do
+	    e.next
+end
 puts e.next
 puts e.next
 e.rewind.next
-e.next
-e.next
+2.times do
+	    e.next
+end
 puts e.next
 puts e.rewind.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
+9.times do
+	    e.next
+end
 puts e.next
 puts e.rewind.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
+6.times do
+	    e.next
+end
 puts e.next
 e.rewind.next
-e.next
-e.next
-e.next
-e.next
-e.next
+5.times do
+	    e.next
+end
 puts e.next
 e.next
 puts e.next
 e.rewind.next
-e.next
-e.next
-e.next
-e.next
+4.times do
+	    e.next
+end
 puts e.next
-e.next
-e.next
-e.next
+3.times do
+	    e.next
+end
 puts e.next
 puts e.rewind.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
+9.times do
+	    e.next
+end
 puts e.next
 puts e.rewind.next
 puts e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
+7.times do
+	    e.next
+end
 puts e.next
 e.rewind.next
-e.next
-e.next
+2.times do
+	    e.next
+end
 puts e.next
-e.next
-e.next
-e.next
+3.times do
+	    e.next
+end
 puts e.next
 puts e.rewind.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
+9.times do
+	    e.next
+end
 puts e.next
 puts e.rewind.next
 e.next
 puts e.next
 e.rewind.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
+7.times do
+	    e.next
+end
 puts e.next
 e.rewind.next
-e.next
-e.next
-e.next
-e.next
-e.next
+5.times do
+	    e.next
+end
 puts e.next
 e.rewind.next
-e.next
-e.next
-e.next
+3.times do
+	    e.next
+end
 puts e.next
 e.rewind.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
+9.times do
+	    e.next
+end
 puts e.next
 puts e.rewind.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
-e.next
+9.times do
+	    e.next
+end
 puts e.next
